04f6502667e0a9f8ac1aec565f8f284a96bfe065
Joao Vitor <=>
2018-05-15 10:44:21 -0300

    Extraindo email do token e armazenando em localStorage

--- src/interceptor/error-interceptor.ts
+++ src/interceptor/error-interceptor.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; //import atualizado
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
