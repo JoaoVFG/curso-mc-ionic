702b2480ef72e98bafcec5268d79b08cfd56a0f6
Joao Vitor <=>
2018-05-23 21:45:06 -0300

    Tela de escolha de endereço

---
+++ src/models/endereco.dto.ts
@@ -0,0 +1,9 @@
+export interface EnderecoDto {
+    id : string;
+    logradouro : string;
+    numero : string;
+    complemento : string;
+    bairro: string;
+    cep : string;
+    cidade : string;
+}
